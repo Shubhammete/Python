1a9db64dab5e16bc4b6bcbb1125a3378129bd64a
Shubham Mete <113412785+Shubhammete@users.noreply.github.com>
2022-11-26 21:52:47 +0530

    Add files via upload

---
+++ Calculator_if_else_.py
@@ -0,0 +1,25 @@
+#Basic calculator using if elif conditional statement
+
+
+s='''
+   Enter the operation
+   Addition +
+   Subtraction -
+   Multipilcation *
+   Division /'''
+print(s)
+a=int(input("num 1 = "))
+b=int(input("num 2 = "))
+c=input("Enter Operation")
+
+if c=="+":
+    print(a+b)
+elif c=="-":
+    print(a-b)
+elif c=="*":
+    print(a*b)
+elif c=="/":
+    print(a/b)
+else:
+    print("Enter Valid operator")
+    

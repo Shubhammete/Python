ee63a63689d96a2071a2c2cf2a2cd7d91458d631
Shubham Mete <113412785+Shubhammete@users.noreply.github.com>
2023-01-06 11:29:14 +0530

    Delete 5. Loops directory

--- 5. Loops/if.py
+++
@@ -1,5 +0,0 @@
-# if[condition expression]:
-#     [statement to execute]
-a=eval(input("Enter the number:  "))
-if a%2==0:
-    print(a,"Even Number")
--- 5. Loops/if_else.py
+++
@@ -1,9 +0,0 @@
-# if[condition expression]:
-#     [statement to execute]
-# else
-#     [alternate statement to execute]
-a=eval(input("Enter the number:  "))
-if a%2==0:
-    print(a,"Even Number")
-else:
-    print(a,"Odd Number")
